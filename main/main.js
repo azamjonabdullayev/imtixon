cb6f8b22d685d64b65af205b5a02944d7b42d1ce
abdullayeff <110798985+azamjonabdullayev@users.noreply.github.com>
2022-10-08 20:58:10 +0500

    innnertial commit

---
+++ main/main.js
@@ -0,0 +1,69 @@
+let ism = "A'zamjon Abdullayev"
+function qisqaIsm(arr){
+    let str1 = ism.split(" ").toLocaleUpperCase
+    let str2 = " "
+     for (let i = 0; i < arr.length; i++) {
+        console.log(str1);
+     }
+     return str2
+}
+console.log(qisqaIsm(arr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let text = ["Keep", "remove" "keep"]
+// function toqQaytar(arr) {
+//     let son = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i % 2 === 0) {
+//             son.push(arr[i]);
+//         }
+//         return son
+
+//     }
+    
+// }
+// console.log(toqQaytar(text));
+
+
+
+
+
+
+
+
+
+
+
